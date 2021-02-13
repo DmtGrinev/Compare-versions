97814a4e4c7164faada5fc75343c60ab9c97af0f
Grinev Dmitry <Grinev.dmt@gmail.com>
2021-02-13 14:07:56 +0500

    Initial Commit

---
+++ Compare versions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compare versions
+//
+//  Created by Tim Grinev on 13.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
